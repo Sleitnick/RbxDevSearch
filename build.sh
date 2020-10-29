7bcfdc93719b0b88644593b024862a874fd8ba46
Stephen Leitnick <sleitnick@gmail.com>
2020-10-29 17:20:02 -0400

    Move page fetch to build

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
+echo "Starting build"
+
 if [ -f chrome_extension_build.zip ]; then
+	echo "Cleaning up old build"
 	rm chrome_extension_build.zip
 fi
 
+echo "Fetching page mapping"
+python3 fetch_page_mapping.py
+
+echo "Zipping Chrome extension"
 cd chrome_extension
 zip -r ../chrome_extension_build.zip *
 
