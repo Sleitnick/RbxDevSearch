7d91a0ed4fcc7838d245f12cc369cf84d862a3f0
Stephen Leitnick <sleitnick@gmail.com>
2020-10-29 16:29:31 -0400

    Theme

--- extension/simpleStorage.js
+++ extension/simpleStorage.js
@@ -1,6 +1,22 @@
-const simpleStorage = {};
+const simpleStorage = {__mock: false};
+
+if (typeof chrome.storage === "undefined") {
+	simpleStorage.__mock = true;
+	simpleStorage.__mockStore = {};
+	simpleStorage.__mockOnChangedCallbacks = [];
+	console.warn("Using mock storage");
+}
 
 simpleStorage.get = (key, defaultValue) => {
+	if (simpleStorage.__mock) {
+		return new Promise((resolve) => {
+			let v = simpleStorage.__mockStore[key];
+			if (typeof v === "undefined") {
+				v = defaultValue;
+			}
+			resolve(v);
+		});
+	}
 	return new Promise((resolve, reject) => {
 		try {
 			chrome.storage.sync.get({[key]: defaultValue}, (value) => resolve(value[key]));
@@ -11,6 +27,16 @@ simpleStorage.get = (key, defaultValue) => {
 };
 
 simpleStorage.set = (key, value) => {
+	if (simpleStorage.__mock) {
+		return new Promise((resolve) => {
+			const oldValue = simpleStorage.__mockStore[key];
+			if (oldValue !== value) {
+				simpleStorage.__mockStore[key] = value;
+				simpleStorage.onChanged.fireAll(key, value, oldValue);
+			}
+			resolve();
+		});
+	}
 	return new Promise((resolve, reject) => {
 		try {
 			chrome.storage.sync.set({[key]: value}, resolve);
@@ -19,3 +45,21 @@ simpleStorage.set = (key, value) => {
 		}
 	});
 };
+
+if (simpleStorage.__mock) {
+	simpleStorage.onChanged = {
+		addListener: (listener) => {
+			simpleStorage.__mockOnChangedCallbacks.push(listener);
+		},
+		fireAll: (key, newValue, oldValue) => {
+			const v = {
+				[key]: {newValue, oldValue}
+			};
+			for (const listener of simpleStorage.__mockOnChangedCallbacks) {
+				listener(v);
+			}
+		}
+	};
+} else {
+	simpleStorage.onChanged = chrome.storage.sync.onChanged;
+}
