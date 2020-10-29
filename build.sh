f37dc4b0867599305cb7bae403d06402e2b82592
Stephen Leitnick <sleitnick@gmail.com>
2020-10-29 17:47:13 -0400

    Restructure

--- build.sh
+++ build.sh
@@ -7,19 +7,21 @@ if [ -f firefox_extension_build.zip ]; then
 	rm firefox_extension_build.zip
 fi
 
-echo "Distributing manifest"
-cp manifest.json chrome_extension/manifest.json
-cp manifest.json firefox_extension/manifest.json
+echo "Distribute shared"
+cp extension/shared/. extension/chrome_extension/
+cp extension/shared/. extension/firefox_extension/
 
 echo "Fetching page mapping"
 python3 fetch_page_mapping.py
 
 echo "Zipping Chrome extension"
-cd chrome_extension
-zip -r ../chrome_extension_build.zip *
+cd extension/chrome_extension
+zip -r ../../chrome_extension_build.zip *
+cd ../..
 
 echo "Zipping Firefox extension"
-cd firefox_extension
-zip -r ../firefox_extension_build.zip *
+cd extension/firefox_extension
+zip -r ../../firefox_extension_build.zip *
+cd ../..
 
 echo "Build complete"
