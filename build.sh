d8530b1dd1e9b5b7079976bdc3b61d4eb36ecff1
Stephen Leitnick <sleitnick@gmail.com>
2020-10-29 18:41:28 -0400

    Restructure

--- build.sh
+++ build.sh
@@ -1,27 +1,15 @@
 echo "Starting build"
 
-if [ -f chrome_extension_build.zip ]; then
-	rm chrome_extension_build.zip
+if [ -f extension_build.zip ]; then
+	rm extension_build.zip
 fi
-if [ -f firefox_extension_build.zip ]; then
-	rm firefox_extension_build.zip
-fi
-
-echo "Distribute shared"
-cp -a extension/shared/. extension/chrome_extension/
-cp -a extension/shared/. extension/firefox_extension/
 
 echo "Fetching page mapping"
 python3 fetch_page_mapping.py
 
-echo "Zipping Chrome extension"
-cd extension/chrome_extension
-zip -r ../../chrome_extension_build.zip *
-cd ../..
-
-echo "Zipping Firefox extension"
-cd extension/firefox_extension
-zip -r ../../firefox_extension_build.zip *
-cd ../..
+echo "Zipping extension"
+cd extension
+zip -r ../extension_build.zip *
+cd ..
 
 echo "Build complete"
